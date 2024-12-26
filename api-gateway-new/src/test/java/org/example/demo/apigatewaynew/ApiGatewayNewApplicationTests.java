7b1ddca51c37f5f6565c9afd3425f89c5ff2ff3f
Кузьминых Сергей <kuzminyhsa@gmail.com>
2024-12-27 07:13:48 +0800

    first commit

---
+++ api-gateway-new/src/test/java/org/example/demo/apigatewaynew/ApiGatewayNewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demo.apigatewaynew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
